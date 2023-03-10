92b8c5c3a6d88f7259ccbc772711ac2330882782
Ayo Sam <ayotolusam@gmail.com>
2023-03-11 00:37:15 +0100

    Improved styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import Navbar from '../Navbar/Navbar'
 import Cart from '../Cart/Cart'
 import { useCart } from '../../contexts/CartContext'
 import logoAlt from '../../assets/logo-alt.png';
-import { FiMail, FiTwitter,  } from 'react-icons/fi';
+import { FiMail, FiTwitter, FiPhone } from 'react-icons/fi';
 import { RiWhatsappLine  } from 'react-icons/ri';
 
 
@@ -32,32 +32,31 @@ const Layout = ({children}: LayoutProps) => {
           <img src={logoAlt} alt="logo" />
           <p>Ojuolape House, 9 NERDC Rd, CBD, Alausa-Ikeja, Lagos</p>
           <div className='socialIcons'>
+            <FiPhone/>
             <FiMail/>
             <RiWhatsappLine/>
             <FiTwitter/>
           </div>
         </div>
-        <div className='helpAbout'>
-          <div className='help'>
-            <h3>Get Help</h3>
-            <ul>
-              <li>Contact us</li>
-              <li>About us</li>
-              <li>Return policy</li>
-              <li>Privacy policy</li>
-              <li>Payment policy</li>
-            </ul>
-          </div>
-          <div className='about'>
-            <h3>About us</h3>
-            <ul>
-              <li>News</li>
-              <li>Service</li>
-              <li>Our Policy</li>
-              <li>Customer care</li> 
-              <li>Faq's</li>
-            </ul>
-          </div>
+        <div className='help'>
+          <h3>Get Help</h3>
+          <ul>
+            <li>Contact us</li>
+            <li>About us</li>
+            <li>Return policy</li>
+            <li>Privacy policy</li>
+            <li>Payment policy</li>
+          </ul>
+        </div>
+        <div className='about'>
+          <h3>About us</h3>
+          <ul>
+            <li>News</li>
+            <li>Service</li>
+            <li>Our Policy</li>
+            <li>Customer care</li> 
+            <li>Faq's</li>
+          </ul>
         </div>
       </footer>
     </div>
